cfc6e629fa9999b782fc274356bc7419d7b1f0d9
KevinSchinaider <kevinschinaider05@outlook.com>
2022-09-17 10:19:55 -0300

    Solucão exercício

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("O que é, o que é...");
+Console.WriteLine("... que o nadador faz para bater o recorde?"); Thread.Sleep(5000);
+Console.WriteLine("");
+Console.WriteLine("Nada.");
+
